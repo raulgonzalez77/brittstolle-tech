c4dcda7efb91e1215f075d6ed35bf8b857c0c6af
Raul Gonzalez <54822905+raulgonzalez77@users.noreply.github.com>
2026-05-01 09:40:28 -0500

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//document.querySelector('my-vid').defaultPlayRate = 2;
+
+ScrollReveal().reveal('.headline');
+ScrollReveal().reveal('.paragraph-one-img', {
+    delay: 200,
+    distance: '30px',
+    origin: 'bottom',
+    opacity: 0,
+    easing: 'ease-out',
+    duration: 800
+});
